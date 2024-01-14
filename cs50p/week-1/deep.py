d1cd22e926ce7409f5bd30e447bac54ebbecae99
chfenja <chfenja@chfenja.com>
2024-01-14 08:47:10 +0000

    CS50P: add deep thought problem

---
+++ cs50p/week-1/deep.py
@@ -0,0 +1,10 @@
+def main():
+    answer = input("What's the answer to the Great Question of Life, the Universe and Everything? ")
+    answer = answer.lower().strip()
+    match answer:
+        case "42" | "forty-two" | "forty two":
+            print("Yes")
+        case _:
+            print("No")
+
+main()
